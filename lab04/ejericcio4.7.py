c5c635d017fe0200a9294da65bfa9477792a4b7e
Aidan Ramon Villanueva (24075) <aidan.villanueva@est.zamorano.edu>
2022-09-28 22:17:31 -0600

    Version 4

--- lab04/ejericcio4.7.py
+++ lab04/ejericcio4.7.py
@@ -1,19 +1,15 @@
-def calcularScore(score):
-    if(score > 0.9 and score <= 1.0): 
-        grade = "A"
-    elif(score > 0.8 and score < 0.9):
-        grade = "B"
-    elif(score > 0.7 and score < 0.8):
-        grade = "C"
-    elif(score > 0.6 and score < 0.7):
-        grade = "D"
-    elif(score < 0 and score < 0.6): 
-        grade = "F"
-    else:
-        grade = "Score no es valido"
-    return grade
-try:
-    score = float(input("Ingrese su score (0.1 - 1.00): "))
-    grade = calcularScore(score)
-except: 
-    print("Error, debe ingrsar un valor numerico")
+puntos = float (input("Ingrese puntuación: "))
+
+if puntos < 0.0 or puntos > 1.0: 
+    print("fuera de rango!!..Debe ser entre 0.0 y 1.0")
+else: 
+    if puntos > 0.9: 
+      print("Sobresaliente")
+    elif puntos >= 0.8 and puntos < 0.9:
+      print("Notable")
+    elif puntos >= 0.7 and puntos < 0.8:
+      print("Bien")
+    elif puntos >= 0.6 and puntos < 0.7:
+      print("Sufficiente")
+    else: 
+      print("Insuficiente")
